1b6ac6982cbcced9433bd52bfa6a6eb100452a6b
mrarikan01 <114260038+mrarikan01@users.noreply.github.com>
2023-01-14 17:05:37 +0000

    Create Browni Degerleri.py

---
+++ Browni Degerleri.py
@@ -0,0 +1,3 @@
+print (" Ürün İsmi=Browni /n Marka=Eti /n Çıkış Yılı=1903 /n Fabrika=Ülkenin Her Yerinde Var /n İletişi No=0353521 /n Protein=4.7 /n Yağ=23.2 /n Lif= 1,8 /n Doymuşyağ= 10.9 /n Tuz= 0.9 /n Şeker= 30.2 /n Trans Yağ= 0.04 /n Karbonhidrat = 54.2 ")
+
+#Az Yenirse Kilo Alınmaz
